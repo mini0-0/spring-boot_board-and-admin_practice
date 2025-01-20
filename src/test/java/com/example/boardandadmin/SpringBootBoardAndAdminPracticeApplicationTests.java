27043f8fe66660842a7aa58b1947aa623e9277b6
mini0-0 <nalala8200@naver.com>
2025-01-20 16:10:07 +0900

    gitflow-feature-stash: 1-init-boot
    https://github.com/djkeh/fastcampus-project-board-admin/issues/2 에 있는 것처럼
    spring boot 초기세팅 함

---
+++ src/test/java/com/example/boardandadmin/SpringBootBoardAndAdminPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boardandadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBoardAndAdminPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
