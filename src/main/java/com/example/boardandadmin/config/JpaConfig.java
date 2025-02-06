852825b065be0bc0c209165b010da4782f3d69be
mini0-0 <nalala8200@naver.com>
2025-02-06 14:22:39 +0900

    # 20 - 회원 관리 기능 컨트롤러, 서비스, 뷰 구현

--- src/main/java/com/example/boardandadmin/config/JpaConfig.java
+++ src/main/java/com/example/boardandadmin/config/JpaConfig.java
@@ -1,12 +1,12 @@
 package com.example.boardandadmin.config;
 
 import com.example.boardandadmin.dto.security.BoardAdminPrincipal;
-import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
@@ -18,11 +18,12 @@ public class JpaConfig {
     @Bean
     public AuditorAware<String> auditorAware() {
         return () -> Optional.ofNullable(SecurityContextHolder.getContext())
-                .map(context -> context.getAuthentication())
+                .map(SecurityContext::getAuthentication)
                 .filter(Authentication::isAuthenticated)
                 .map(Authentication::getPrincipal)
                 .map(BoardAdminPrincipal.class::cast)
                 .map(BoardAdminPrincipal::getUsername);
+
     }
 
 }
